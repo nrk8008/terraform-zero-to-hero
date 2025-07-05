9b939a6c48d9be08177ff1a8ec4ea7d373335c37
Rajesh <nrk8008@gmail.com>
2025-07-05 23:28:57 +0530

    update files

--- Day-01/project-ec2-instance-creation/main.tf
+++ Day-01/project-ec2-instance-creation/main.tf
@@ -5,4 +5,7 @@ provider "aws" {
 resource "aws_instance" "my-aws-instance" {
     ami           = "ami-0f918f7e67a3323f0"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = "subnet-0daec53321083b10a"
+    key_name = "cicd-demo"
+    
 }
